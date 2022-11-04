304d3cb973316a6ed7c6c4b4b7261039655f6147
Zenn-Anjay <110921035+Zenn-Anjay@users.noreply.github.com>
2022-11-04 14:27:50 +0700

    Update hello.js

--- plugins/hello.js
+++ plugins/hello.js
@@ -6,9 +6,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
 conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
+    mediaUrl: "https://www.youtube.com/channel/UCp3j5bRzkSa2BN3kxdVrN8A",
     mediaType: 2,
-    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
+    description: "https://www.youtube.com/channel/UCp3j5bRzkSa2BN3kxdVrN8A", 
     title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
     body: wm,
     thumbnail: thumb,
